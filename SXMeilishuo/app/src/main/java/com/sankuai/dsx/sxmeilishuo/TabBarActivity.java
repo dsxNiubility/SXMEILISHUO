82fd11c0f683f3bfe9437cad050e58c88d5050bb
DongShangXian <dantesx2012@gmail.com>
2016-10-18 16:25:30 +0800

    底部tabbar美化，还缺细节

--- SXMeilishuo/app/src/main/java/com/sankuai/dsx/sxmeilishuo/TabBarActivity.java
+++ SXMeilishuo/app/src/main/java/com/sankuai/dsx/sxmeilishuo/TabBarActivity.java
@@ -5,49 +5,65 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.sankuai.dsx.sxmeilishuo.R;
 /**
  * Created by dsx on 16/10/18.
+ * index
  */
 
 public class TabBarActivity extends FragmentActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+    //定义FragmentTabHost对象
+    private FragmentTabHost mTabHost;
+    private LayoutInflater layoutInflater;
+
+    private Class fragmentArray[] = {HomeFragment.class,ShoppingFragment.class,ShoppingFragment.class,MessageFragment.class,MyInfoFragment.class};
+    private int mImageViewArray[] = {R.drawable.tabhost_home,R.drawable.tabhost_shop,R.drawable.tabhost_shop,
+            R.drawable.tabhost_msg,R.drawable.tabhost_my};
+    private String mTextviewArray[] = {"首页", "购物", "相机", "消息", "我的"};
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabchoose);
 
-        TabHost tabhost = (TabHost) findViewById(R.id.mytabhost);
+        initView();
+    }
 
-        tabhost.setup();
-        tabhost.addTab(tabhost.newTabSpec("one").setIndicator("首页",getResources().getDrawable(R.drawable.tabhost_home)).setContent(R.id.widget_layout_home));
-        tabhost.addTab(tabhost.newTabSpec("two").setIndicator("购物").setContent(R.id.widget_layout_shopping));
-        tabhost.addTab(tabhost.newTabSpec("middle").setIndicator("相机").setContent(R.id.widget_layout_shopping));
-        tabhost.addTab(tabhost.newTabSpec("three").setIndicator("消息").setContent(R.id.widget_layout_message));
-        tabhost.addTab(tabhost.newTabSpec("four").setIndicator("我的").setContent(R.id.widget_layout_myinfo));
+    /**
+     * 初始化组件
+     */
+    private void initView(){
+        //实例化布局对象
+        layoutInflater = LayoutInflater.from(this);
+        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        int count = fragmentArray.length;
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransactionHome = fragmentManager.beginTransaction();
-        FragmentTransaction fragmentTransactionShopping = fragmentManager.beginTransaction();
-        FragmentTransaction fragmentTransactionMessage = fragmentManager.beginTransaction();
-        FragmentTransaction fragmentTransactionMyInfo = fragmentManager.beginTransaction();
+        for(int i = 0; i < count; i++){
+            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTextviewArray[i]).setIndicator(getTabItemView(i));
+            mTabHost.addTab(tabSpec, fragmentArray[i], null);
+        }
+    }
 
-        HomeFragment fragmentHome = new HomeFragment();
-        fragmentTransactionHome.add(R.id.widget_layout_home, fragmentHome);
-        fragmentTransactionHome.commit();
+    /**
+     * 给Tab按钮设置图标和文字
+     */
+    private View getTabItemView(int index){
+        View view = layoutInflater.inflate(R.layout.tab_item_view, null);
 
-        ShoppingFragment fragmentShop = new ShoppingFragment();
-        fragmentTransactionShopping.add(R.id.widget_layout_shopping, fragmentShop);
-        fragmentTransactionShopping.commit();
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
+        imageView.setImageResource(mImageViewArray[index]);
 
-        MessageFragment fragmentMsg = new MessageFragment();
-        fragmentTransactionMessage.add(R.id.widget_layout_message, fragmentMsg);
-        fragmentTransactionMessage.commit();
+        TextView textView = (TextView) view.findViewById(R.id.textview);
+        textView.setText(mTextviewArray[index]);
 
-        MyInfoFragment fragmentMy = new MyInfoFragment();
-        fragmentTransactionMyInfo.add(R.id.widget_layout_myinfo, fragmentMy);
-        fragmentTransactionMyInfo.commit();
-
+        return view;
     }
 }
