05d2fa9a3ec40c3f1cde4f5c479a2c0867188a8d
DongShangXian <dantesx2012@gmail.com>
2016-10-24 19:44:02 +0800

    轮播图ok

---
+++ SXMeilishuo/app/src/main/java/com/sankuai/dsx/sxmeilishuo/GlideImageLoader.java
@@ -0,0 +1,18 @@
+package com.sankuai.dsx.sxmeilishuo;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * Created by dsx on 16/10/24.
+ */
+
+public class GlideImageLoader implements ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context).load(path).into(imageView);
+    }
+}
--- SXMeilishuo/app/src/main/java/com/sankuai/dsx/sxmeilishuo/HomeFragment.java
+++ SXMeilishuo/app/src/main/java/com/sankuai/dsx/sxmeilishuo/HomeFragment.java
@@ -1,21 +1,39 @@
 package com.sankuai.dsx.sxmeilishuo;
 
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerClickListener;
+
+import java.util.Arrays;
 
 /**
  * Created by dsx on 16/10/18.
  */
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment  {
+
+    Banner mBanner;
+    String[] images,titles;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        images = getResources().getStringArray(R.array.url);
+        titles = getResources().getStringArray(R.array.title);
+        mBanner = (Banner)getActivity().findViewById(R.id.top_banner);
+
     }
 
     @Nullable
@@ -27,6 +45,25 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        images = getResources().getStringArray(R.array.url);
+        titles = getResources().getStringArray(R.array.title);
+        mBanner = (Banner)view.findViewById(R.id.top_banner);
+
+        //简单使用
+        mBanner.setImages(Arrays.asList(images)).setImageLoader(new GlideImageLoader()).start();
+
+        //设置banner样式
+        mBanner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+        mBanner.isAutoPlay(true);
+        mBanner.setDelayTime(3000);
+        mBanner.setIndicatorGravity(BannerConfig.CENTER);
+        mBanner.start();
+        mBanner.setOnBannerClickListener(new OnBannerClickListener() {
+            @Override
+            public void OnBannerClick(int position) {
+                Toast.makeText(getActivity().getApplicationContext(), "点击：" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -43,10 +80,22 @@ public class HomeFragment extends Fragment {
         System.out.println("HomeFragment--onResume");
     }
 
+
+    //如果你需要考虑更好的体验，可以这么操作
     @Override
-    public void onStop()
-    {
-        super.onStop();
-        System.out.println("HomeFragment--onStop");
+    public void onStart() {
+        super.onStart();
+        Log.i("--", "onStart");
+        //开始轮播
+        mBanner.startAutoPlay();
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i("--", "onStop");
+        //结束轮播
+        mBanner.stopAutoPlay();
+    }
+
 }
